35a063f18190734533d942f445d7189032b516f4
Duylam Nguyen-Ngo <dn5jg@virginia.edu>
2013-07-11 02:40:00 -0700

    Cannot get tooltips to work. Hmm

--- chrome/datamap.js
+++ chrome/datamap.js
@@ -1,26 +1,26 @@
-function plotter(meow){
-$(document).ready(function() {
-    var ctx = $("#chart");
-    var temp = ctx[0].getContext("2d");
-    var preData = JSON.parse(meow);
-    var data = [
-        {value: preData["facebook"],
-         color: "#F7464A"},
-        {value: preData["youtube"],
-         color: "#E2EAE9"},
-        {value: preData["reddit"], 
-         color: "#D4CCC5"},
-        {value: preData["twitter"],
-         color: "#949FB1"},
-        {value: preData["techcrunch"], 
-         color: "#4D5360"},
-        {value: preData["instagram"], 
-         color: "#2E2360"},
-        {value: preData["news.ycombinator"], 
-         color: "#2D6FAE"},
-        {value: preData["allother"], 
-         color: "#000000"}
-    ];
-    var chart0 = new Chart(temp).Doughnut(data);
-});
-}
+// function plotter(meow){
+// $(document).ready(function() {
+//     var ctx = $("#chart");
+//     var temp = ctx[0].getContext("2d");
+//     var preData = JSON.parse(meow);
+//     var data = [
+//         {value: preD["tumblr"],
+//          color: "#32506d"},
+//         {value: preData["reddata["facebook"],
+//          color: "#3B5998"},
+//         {value: preData["youtube"],
+//          color: "#CD332D"},
+//         {value: preDatait"], 
+//          color: "#114475"},
+//         {value: preData["twitter"],
+//          color: "#4099FF"},
+//         {value: preData["techcrunch"], 
+//          color: "#008C00"},
+//         {value: preData["news.ycombinator"], 
+//          color: "#FF6500"},
+//         {value: preData["allother"], 
+//          color: "#EDCC92"}
+//     ];
+//     var chart0 = new Chart(temp).Doughnut(data);
+// });
+// }
--- chrome/history.js
+++ chrome/history.js
@@ -34,7 +34,7 @@ var blacklist = {"facebook":0,
   "reddit":0,
   "twitter":0,
   "techcrunch":0,
-  "instagram":0,
+  "tumblr":0,
   "news.ycombinator":0,
   "allother":0};
 
@@ -74,20 +74,22 @@ var blacklist = {"facebook":0,
         color: gaycolor};
         chart_data.push(entry);
     });
-    console.log(i_rest);
-    console.log(rest);
-    console.log(top);
+    // console.log(i_rest);
+    // console.log(rest);
+    // console.log(top);
 
     var ctx = $("#chart");
+
     var temp = ctx[0].getContext("2d");
     new Chart(temp).Doughnut(chart_data);
   }
 
   function make_exp_bar(results){
     freq_list = hash_to_freqs(results);
-    console.log(freq_list);
+
     freq_list = freq_list.sort(function(a,b) {return b[1] - a[1];});
-    var top = freq_list.slice(0,30)
+    // console.log("thisisit "+freq_list);
+    var top = freq_list.slice(0,10)
     var lbls = []
     top.forEach(function(item){lbls.push(item[0]);});
     var data = []
@@ -97,30 +99,61 @@ var blacklist = {"facebook":0,
       labels : lbls,
       datasets : [
         {
-        fillColor : "rgba(220,220,220,0.5)",
+        fillColor : "rgba(100,100,100,0.5)",
         strokeColor : "rgba(0,0,255,1)",
         data : data
       },
       ],
     };
+
+    var options = {
+      scaleShowLabels : true,
+      scaleLineWidth : 3
+    };
+
+    var value ="YO";
     var ctx = $("#expchart");
     var temp = ctx[0].getContext("2d");
-    new Chart(temp).Bar(chart_data);
+    new Chart(temp).Bar(chart_data,{scaleLineWidth:3,scaleLabel:"<%=value%>"});
   }
 
-
-
+function plotter(meow){
+$(document).ready(function() {
+    var ctx = $("#chart");
+    var temp = ctx[0].getContext("2d");
+    var preData = JSON.parse(meow);
+    var data = [
+        {value: preData["tumblr"],
+         color: "#32506d"},
+        {value: preData["facebook"],
+         color: "#3B5998"},
+        {value: preData["youtube"],
+         color: "#CD332D"},
+        {value: preData["reddit"], 
+         color: "#114475"},
+        {value: preData["twitter"],
+         color: "#4099FF"},
+        {value: preData["techcrunch"], 
+         color: "#008C00"},
+        {value: preData["news.ycombinator"], 
+         color: "#FF6500"},
+        {value: preData["allother"], 
+         color: "#EDCC92"}
+    ];
+    var chart0 = new Chart(temp).Doughnut(data);
+});
+}
 
   function make_donut(results){
     for(var i = 0;i<results.length;i++){
       clean_url(results[i].url);
     }
-    console.log(blacklist);
+    // console.log(blacklist);
     plotter(JSON.stringify(blacklist));
   }
 
   function clean_url(url){
-    var blackList = "(facebook|youtube|reddit|twitter|techcrunch|instagram|news\.ycombinator)(.com)"
+    var blackList = "(facebook|youtube|reddit|twitter|techcrunch|tumblr|news\.ycombinator)(.com)"
     var matchURL = new RegExp(blackList);
     matchURL.pass = matchURL.test(url),
     matchURL.url = matchURL.exec(url);
@@ -142,7 +175,7 @@ var blacklist = {"facebook":0,
     var data = []
     var tmp = new Date(0);
     var yesterday = tmp.getDay();
-    console.log(full_hist.length);
+    // console.log(full_hist.length);
     for (var i = 0; i < full_hist.length; i++){
       var visit = full_hist[i];
       curday = (new Date(visit.visitTime)).getDay();
@@ -166,17 +199,21 @@ var blacklist = {"facebook":0,
     for (var i = 0; i < data.length; i++){
       data[i] = 0;
     }
+    console.log(full_hist.length);
     for (var j = 1; j < full_hist.length; j++){
       msecs = full_hist[j].visitTime;
       hour = (new Date(msecs)).getHours();
+      // console.log("This is hour: "+hour);
       data[hour] += 1;
     }
+    
     return data;
   }
 
   function time_series(full_hist){
+    // console.log(full_hist);
     data = bucket_hours(full_hist)
-    console.log(data.length);
+    // console.log(data.length);
     var lbls = [];
     for (var m = 1; m <= data.length; m++) {
       lbls.push(m);
@@ -191,6 +228,7 @@ var blacklist = {"facebook":0,
       },
       ],
     };
+    console.log(data);
     var ctx = $('#timeseries')[0].getContext('2d');
     new Chart(ctx).Bar(data);
   }
@@ -210,15 +248,14 @@ var blacklist = {"facebook":0,
                                     }
                                  });
                                });
-    });
-
+    }); 
     setTimeout(function(){
-      console.log(full_hist.length);
+      // console.log(full_hist.length);
       /************************************
       * Main Graph creating funcs are here*
       *************************************/
       time_series(full_hist);
-      make_v2_donut(results);
+      make_donut(results);
       make_exp_bar(results);
     }, 1000);
   }
